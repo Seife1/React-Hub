00b2fae0db326e554130882573740e481d916251
Seife1 <gebrealabate19@yahoo.com>
2023-12-18 14:51:17 +0300

    text updated to img

--- _2Conditional/src/App.jsx
+++ _2Conditional/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import './App.css';
 
+import elul from '../../Assets/elul.jpg';
+
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div className="card">
-        <h1>{count % 2 !== 0 ? "Hello" : "Hello, Eden"}</h1>
+        <h1>{(count % 2 !== 0) ? "Hello, Eden" : (<img src= {elul} alt="image" />)}</h1>
+        <caption>{(count % 2 === 0) ? "Eden Sisay" : " "}</caption>
         <button onClick={() => setCount((count) => count + 1)}>
           Click Me
         </button>
